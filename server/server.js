9cd441cb8119c6ee4a6a8de3ffe64f13d26636cd
181 <60948233+roman181@users.noreply.github.com>
2026-03-30 13:43:56 +0200

    Set up Express and Socket.io for multiplayer game server

---
+++ server/server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.send('<h1>Multiplayer Game Server</h1>');
+});
+
+io.on('connection', (socket) => {
+    console.log('A user connected');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
